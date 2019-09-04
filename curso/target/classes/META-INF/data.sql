79dc74b7d582137bca062ff97c50960fc3a3a7b7
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2019-09-04 12:29:32 +0200

    hla

--- curso/target/classes/META-INF/data.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT into Book (title,author,date) values ('Anna Karenina','Tolstoi','1918-02-12');
-INSERT into Book (title,author,date) values ('War and Peace','Tolstoi','1948-08-03');
-INSERT into Page (text,font,book_title) values ('Happy families are all alike; every unhappy family is unhappy in its own way.','nunito','Anna Karenina');
-INSERT into Page (text,font,book_title) values ('Everything was in confusion in the Oblonskys'' house.','nunito','Anna Karenina');
-INSERT into Page (text,font,book_title) values ('The wife had discovered that the husband was carrying on an intrigue with a French girl, who had been a governess in their family, and she had announced to her husband that she could not go on living in the same house with him.','nunito','Anna Karenina');
