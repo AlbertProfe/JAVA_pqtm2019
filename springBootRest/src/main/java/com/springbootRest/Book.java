05a16745da173b2e7a287dbd44083365ebfe9eb4
inspiratalbert <inspiratalbert@DESKTOP-E24ITDK>
2019-09-10 11:33:42 +0200

    First commit, upload

---
+++ springBootRest/src/main/java/com/springbootRest/Book.java
@@ -0,0 +1,52 @@
+package com.springbootRest;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="books")
+public class Book {
+      @Id	
+      private String title;
+	private String ISBN;
+	private int pages;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String Title) {
+		this.title = Title;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String ISBN) {
+		this.ISBN = ISBN;
+	}
+	public int getPages() {
+		return pages;
+	}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+	public Book(String title, String ISBN, int pages) {
+		super();
+		this.title = title;
+		this.ISBN = ISBN;
+		this.pages = pages;
+	}
+
+
+
+
+      public Book(String title) {
+		super();
+		this.title = title;
+		
+	}
+
+	public Book() {
+		super();
+	}	
+	}
---
+++ springBootRest/src/main/java/com/springbootRest/BookService.java
@@ -0,0 +1,37 @@
+package com.springbootRest;
+
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class BookService {
+
+	@Autowired
+	BookRepository repository;
+	
+	
+	public Iterable<Book> findAll() {
+		
+		return repository.findAll();
+	}
+	
+	public void insertBook(Book book) {
+		
+		repository.save(book);
+	}
+	
+	public void deleteBook(Book book) {
+		
+		repository.delete(book);
+	}
+
+}
+
+
+
