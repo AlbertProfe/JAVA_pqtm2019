5f6d197f6b18d75bb82517931f9d44651accab3f
admin CIFO <admin CIFO@DESKTOP-NG7OTN7>
2020-10-05 09:05:31 +0200

    commit2

--- Intro1/src/test2/Hello.java
+++ Intro1/src/test2/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello class, welcome to java world ... :-)");
-		//tag
+		//tag to object
 		Person luis;
 		luis = new Person("Luis", "Gasol", 35);
 		
